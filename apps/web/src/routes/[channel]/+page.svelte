299a95e13a9e08d67536ea3644b0e72cdb6a06cb
kysley <evan@kysley.com>
2023-05-28 21:09:58 -0400

    add search filter

--- apps/web/src/routes/[channel]/+page.svelte
+++ apps/web/src/routes/[channel]/+page.svelte
@@ -1,28 +1,46 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import {
-    ClickableTile,
     ContentSwitcher,
     Switch,
     Tile,
+    Search,
+    Pagination,
   } from "carbon-components-svelte";
   import { onMount } from "svelte";
-  import { createQuery, hashQueryKey } from "@tanstack/svelte-query";
+  import { createQuery } from "@tanstack/svelte-query";
+  import { debounce } from "../../lib/debounce";
 
   const DURATIONS = [1, 3, 8, 24];
 
   $: selectedIndex = 0;
+  $: searchFilterDebounced = "";
+  $: searchFilter = "";
   let totals = [];
   let emotes: Record<string, string>;
 
+  const updateDebouncedValue = debounce((value: string) => {
+    searchFilterDebounced = value;
+    console.log("Debounced value:", searchFilterDebounced);
+    // Perform any additional logic with the debounced value
+  }, 300); // Adjust the delay time according to your requirements
+
   $: query = createQuery<{ count: number; emoteName: string }[], Error>(
-    ["since", selectedIndex],
+    ["since", selectedIndex, searchFilterDebounced],
 
     async () => {
       const res = await fetch(
         `http://localhost:8012/channel/moonmoon/since?since=${DURATIONS[selectedIndex]}`
       );
       const data = (await res.json()) as { count: number; emoteName: string }[];
+
+      if (searchFilterDebounced) {
+        return data.filter((e) =>
+          e.emoteName
+            .toLowerCase()
+            .includes(searchFilterDebounced.toLowerCase())
+        );
+      }
 
       return data;
     },
@@ -46,22 +64,32 @@
   });
 </script>
 
-<main class="flex w-full flex-col">
+<main class="flex w-full flex-col p-5">
   <h1>{$page.params.channel}</h1>
-  <div class="md:w-60 w-full">
-    <ContentSwitcher bind:selectedIndex size="sm">
+  <div class="w-full flex flex-row gap-3 items-center justify-between">
+    <ContentSwitcher bind:selectedIndex size="sm" class="w-52">
       <Switch text="1h" />
       <Switch text="3h" />
       <Switch text="8h" />
       <Switch text="24h" />
+      <Switch text="all time" disabled />
     </ContentSwitcher>
+    <Search
+      on:input={(e) => (searchFilter = e.target.value)}
+      on:input={(e) => updateDebouncedValue(e.target.value)}
+    />
   </div>
 
   {#if !$query.isLoading && $query.data && emotes}
     <div class="grid grid-cols-8 gap-3">
       {#each $query.data as dat}
         <Tile>
-          <span class="break-all">{dat.emoteName}</span>
+          <a
+            href={`https://betterttv.com/emotes/${emotes[dat.emoteName]}`}
+            target="_blank"
+          >
+            <span class="break-all">{dat.emoteName}</span>
+          </a>
           <img
             src={`https://cdn.betterttv.net/emote/${
               emotes[dat.emoteName]
@@ -73,16 +101,11 @@
       {/each}
     </div>
   {/if}
-
-  <!-- {#if $query.isLoading} -->
-
-  <!-- {#if totals} -->
-  <!-- <div class="flex flex-col"> -->
-  <!-- {#each totals as doc}
-        <span>
-          {doc.emoteName} - {doc.usageCount}
-        </span>
-      {/each} -->
-  <!-- </div> -->
-  <!-- {/if} -->
 </main>
+<Pagination
+  style="position:sticky; bottom: 0; left: 0;"
+  pageSize={$query.data?.length}
+  totalItems={$query.data?.length}
+  pageSizeInputDisabled
+  pageInputDisabled
+/>
