ad66fb6478ecd8a6b74b6e7b18d84b8a4287190c
kysley <evan@kysley.com>
2023-05-29 21:33:40 -0400

    add table view

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -3,8 +3,9 @@
 </script>
 
 <main class="flex items-center justify-center w-full">
-  <div>
+  <div class="flex gap-3 flex-col">
     <h1 class="font-bold">emote.farm</h1>
     <ClickableTile href="/moonmoon">moonmoon</ClickableTile>
+    <ClickableTile href="/masondota2">masondota2</ClickableTile>
   </div>
 </main>
--- apps/web/src/routes/[channel]/+page.svelte
+++ apps/web/src/routes/[channel]/+page.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import {
-    ContentSwitcher,
-    Switch,
     Tile,
-    Search,
     Pagination,
+    RadioButtonGroup,
+    RadioButton,
   } from "carbon-components-svelte";
   import { onMount } from "svelte";
   import { createQuery } from "@tanstack/svelte-query";
   import { debounce } from "../../lib/debounce";
+  import EmoteTable from "../../lib/components/emote-table.svelte";
 
   const DURATIONS = [1, 3, 8, 24];
 
   $: selectedIndex = 0;
   $: searchFilterDebounced = "";
   $: searchFilter = "";
+  let viewMode = "grid";
   let totals = [];
   let emotes: Record<string, string>;
 
@@ -66,46 +67,60 @@
 
 <main class="flex w-full flex-col p-5">
   <h1>{$page.params.channel}</h1>
-  <div class="w-full flex flex-row gap-3 items-center justify-between">
-    <ContentSwitcher bind:selectedIndex size="sm" class="w-52">
+  <RadioButtonGroup bind:selected={viewMode}>
+    <RadioButton labelText="Grid" value="grid" />
+    <RadioButton labelText="Table" value="table" />
+  </RadioButtonGroup>
+  <!-- <div class="w-full grid grid-cols-12 gap-3 items-center justify-between">
+    <ContentSwitcher bind:selectedIndex size="sm" class="col-span-5">
       <Switch text="1h" />
       <Switch text="3h" />
       <Switch text="8h" />
       <Switch text="24h" />
       <Switch text="all time" disabled />
     </ContentSwitcher>
-    <Search
-      on:input={(e) => (searchFilter = e.target.value)}
-      on:input={(e) => updateDebouncedValue(e.target.value)}
-    />
-  </div>
+    <div class="col-start-10 col-span-3">
+      <Search on:input={(e) => updateDebouncedValue(e.target.value)} />
+    </div>
+  </div> -->
 
   {#if !$query.isLoading && $query.data && emotes}
-    <div class="grid grid-cols-8 gap-3">
-      {#each $query.data as dat}
-        <Tile>
-          <a
-            href={`https://betterttv.com/emotes/${emotes[dat.emoteName]}`}
-            target="_blank"
-          >
-            <span class="break-all">{dat.emoteName}</span>
-          </a>
-          <img
-            src={`https://cdn.betterttv.net/emote/${
-              emotes[dat.emoteName]
-            }/3x.webp`}
-            alt={dat.emoteName}
-          />
-          <span class="font-bold">{dat.count}</span>
-        </Tile>
-      {/each}
-    </div>
+    {#if viewMode === "table"}
+      <EmoteTable
+        data={$query.data?.map((d) => ({
+          ...d,
+          id: d.emoteName,
+          emoteId: emotes[d.emoteName],
+        }))}
+        loading={$query.isLoading}
+      />
+    {:else}
+      <div class="grid grid-cols-8 gap-3">
+        {#each $query.data as dat}
+          <Tile>
+            <a
+              href={`https://betterttv.com/emotes/${emotes[dat.emoteName]}`}
+              target="_blank"
+            >
+              <span class="break-all">{dat.emoteName}</span>
+            </a>
+            <img
+              src={`https://cdn.betterttv.net/emote/${
+                emotes[dat.emoteName]
+              }/3x.webp`}
+              alt={dat.emoteName}
+            />
+            <span class="font-bold">{dat.count}</span>
+          </Tile>
+        {/each}
+      </div>
+    {/if}
   {/if}
+  <Pagination
+    style="position:sticky; bottom: 0; left: 0;"
+    pageSize={$query.data?.length}
+    totalItems={$query.data?.length}
+    pageSizeInputDisabled
+    pageInputDisabled
+  />
 </main>
-<Pagination
-  style="position:sticky; bottom: 0; left: 0;"
-  pageSize={$query.data?.length}
-  totalItems={$query.data?.length}
-  pageSizeInputDisabled
-  pageInputDisabled
-/>
